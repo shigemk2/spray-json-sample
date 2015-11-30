18f2ab324ab8d763fc9fb1ad1ee33b4c47281b51
shigemk2 <i.am.shige@gmail.com>
2015-11-30 23:45:35 +0900

    src

---
+++ src/main/com/Sample.scala
@@ -0,0 +1,16 @@
+package com
+
+import spray.json._
+
+object Sample extends DefaultJsonProtocol {
+
+  def main(args: Array[String]) {
+    val hoge:List[List[String]] = List(
+      List("hoge", "fuga"),
+      List("piyo", "fuga")
+    )
+
+    println(hoge.toJson)
+
+  }
+}
