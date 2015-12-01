3f34244172d4cc0133d7c2fdfcd24621cbf74418
shigemk2 <i.am.shige@gmail.com>
2015-12-02 00:46:48 +0900

    いろいろJSON

--- src/main/scala/Sample.scala
+++ src/main/scala/Sample.scala
@@ -4,10 +4,24 @@ import DefaultJsonProtocol._
 
 object Sample {
   def main(args: Array[String]) {
-    val hoge:List[Map[String,String]] = List(
+    println("------------------------------")
+    val hoge1:List[Map[String,String]] = List(
       Map("hoge"->"fuga"),
       Map("piyo"->"fuga")
     )
-    println(hoge.toJson)
+    println(hoge1.toJson)
+    println("------------------------------")
+    val hoge2 = List(
+      Map("hoge"-> Map("fuga"->"name")),
+      Map("piyo"-> Map("puyo"->"name"))
+    )
+    println(hoge2.toJson)
+    println("------------------------------")
+    val hoge3 = List(
+      Map("hoge"-> List(1,2,3)),
+      Map("piyo"-> List(4,5,6))
+    )
+    println(hoge3.toJson)
+    println("------------------------------")
   }
 }
